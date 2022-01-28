b7d3284a210f2c6fb68e923a6a4092231dc19aef
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-01-29 00:46:31 +0300

    deploy 13

--- app/views/users.py
+++ app/views/users.py
@@ -31,7 +31,7 @@ async def users_current_user(
     return res
 
 
-@router.get("/all", summary="Получить всех пользователей")
+@router.get("/all", summary="Получить всех пользователей test of deploy")
 @router.get("/all/", include_in_schema=False)
 async def users_get_all(db: Session = Depends(get_db)):
     """
